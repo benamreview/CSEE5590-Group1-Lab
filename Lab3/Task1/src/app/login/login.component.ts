96af75fa208218f05e794a6f3f4e20f055c15d56
Evan W <sportsdude716@gmail.com>
2019-04-13 01:25:25 -0500

    Validation message fixes

--- Lab3/Task1/src/app/login/login.component.ts
+++ Lab3/Task1/src/app/login/login.component.ts
@@ -17,8 +17,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(3)]]
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
     });
   }
 
